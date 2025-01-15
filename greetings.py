4b80f739e6c9e1c01f0d4840389d6431adbf837c
K-001-BBT <amz05@mail.ru>
2025-01-15 09:34:39 +0500

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = 'Konstantin'
+title = 'Заметка 1'
+content = 'Задание 2'
+status = 'В работе'
+created_date = '15-01-2025'
+issue_date = '17-01-2025'
+print('имя пользователя',username)
+print('заголовок заметки',title)
+print('описание заметки',content)
+print('статус заметки',status)
+print('дата создания заметки в формате "день-месяц-год"',created_date)
+print('дата истечения заметки (дедлайн) в формате "день-месяц-год"',issue_date)
